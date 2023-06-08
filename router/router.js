67c6465058b9ab18fa4dff8dc8d00873601d9196
Eissanoor <86971746+Eissanoor@users.noreply.github.com>
2023-06-08 12:37:23 +0500

    asdasdsad

--- router/router.js
+++ router/router.js
@@ -45,6 +45,10 @@ router.post(
   "/AddLocationInworkRequestPOST",
   FATSDB.AddLocationInworkRequestPOST
 );
+router.post(
+  "/AddWorkTypeInworkRequestPOST",
+  FATSDB.AddWorkTypeInworkRequestPOST
+);
 //--------------------------------------------------------------------------
 
 //-------------------------------GET_API---------------------------------------------
