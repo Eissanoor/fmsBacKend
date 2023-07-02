4f445cf4aa8eca9c966d385401d6e1a0f7d8e752
Eissanoor <86971746+Eissanoor@users.noreply.github.com>
2023-07-02 16:53:11 +0500

    sadasd

--- controllers/controlletrsMSSQL.js
+++ controllers/controlletrsMSSQL.js
@@ -1353,6 +1353,18 @@ WHERE SolutiontatusCode='${SolutiontatusCode}'`
       res.status(500).json({ error: `${error}` });
     }
   },
+  async Building_LIST(req, res, next) {
+    try {
+      let pool = await sql.connect(config);
+      let data = await pool
+        .request()
+        .query(`select BuildingCode from prmBuilding`);
+      res.status(200).json(data);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: `${error}` });
+    }
+  },
   //-----------------------------------------------------------------------------------
 
   //---------------------------DELETE--------------------------------------------------------
--- router/router.js
+++ router/router.js
@@ -124,6 +124,7 @@ router.get("/Solution_GET_BYID/:SolutiontatusCode", FATSDB.Solution_GET_BYID);
 router.post("/getworkRequest", FATSDB.getworkRequest);
 router.post("/getworkRequestsecond", FATSDB.getworkRequestsecond);
 router.get("/Department_LIST", FATSDB.Department_LIST);
+router.get("/Building_LIST", FATSDB.Building_LIST);
 //------------------------------------------------------------------------------
 //-----------------------------------PUT_API-------------------------------------
 router.put("/WorkTrade_Put/:WorkTypeCode", FATSDB.WorkTrade_Put);
