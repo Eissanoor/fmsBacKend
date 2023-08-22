c3c080330aec59331f66b35a02b3aee3e63854ac
Eissanoor <86971746+Eissanoor@users.noreply.github.com>
2023-08-22 11:27:36 +0500

    sadsad

--- controllers/controlletrsMSSQL.js
+++ controllers/controlletrsMSSQL.js
@@ -2949,6 +2949,17 @@ ON o.RequestNumber=i.RequestNumber`);
       res.status(500).json({ error: `${error}` });
     }
   },
+  async EmployeeID_GET_LIST(req, res, next) {
+    try {
+      let pool = await sql.connect(config);
+      let data = await pool.request().query(`select EmployeeID from tblEmployeeMaster`);
+      res.status(200).json(data);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: `${error}` });
+    }
+  },
+  
   //-----------------------------------------------------------------------------------
 
   //---------------------------DELETE--------------------------------------------------------
--- router/router.js
+++ router/router.js
@@ -227,6 +227,7 @@ router.get("/EmployeeMaster_GET_LIST", FATSDB.EmployeeMaster_GET_LIST)
 router.get("/assetworkrequest_GET_BYID/:RequestNumber", FATSDB.assetworkrequest_GET_BYID)
 router.get("/tblAssetsMaster_GET_BYID/:AssetItemDescription", FATSDB.tblAssetsMaster_GET_BYID)
 router.get("/workRequestCount_GET_BYID/:No", FATSDB.workRequestCount_GET_BYID)
+router.get("/EmployeeID_GET_LIST",FATSDB.EmployeeID_GET_LIST)
 router.post("/getworkRequest_by_EPID",FATSDB.getworkRequest_by_EPID)
 //------------------------------------------------------------------------------
 //-----------------------------------PUT_API-------------------------------------
